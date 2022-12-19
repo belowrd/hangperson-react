50ae4eec9901da15ee319135af6c866952a3e198
Clemens Bülow <clemensbify@gmail.com>
2022-12-19 11:45:10 +0100

    header ok

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,12 @@
-// Hallo whaddup hier?
+import React from "react";
+
+const Header = () => {
+    return (
+        <>
+            <h1>hangperson</h1>
+            <p>find the safe word - try a letter!</p>
+        </>
+    );
+};
+
+export default Header;
