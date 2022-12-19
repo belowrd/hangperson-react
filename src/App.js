d7e8b917c337cf88e1ddf7f0c2bf56a6eb6a58d8
Clemens Bülow <clemensbify@gmail.com>
2022-12-19 11:56:36 +0100

    css updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from "react";
 
 import Header from "./components/Header";
+import Person from "./components/Person";
 
 import "./App.css";
 
@@ -9,6 +10,7 @@ function App() {
     return (
         <>
             <Header />
+            <Person />
         </>
     );
 }
