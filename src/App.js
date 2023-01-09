bf55acba77149bd3dfe637be9b13b404673c0322
Clemens Bülow <clemensbify@gmail.com>
2023-01-09 20:50:47 +0100

    update interface

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
             <GameBoard />
             <Footer />
             <Snowfall
-                snowflakeCount={350}
+                snowflakeCount={35000}
                 style={{
                     position: "fixed",
                     width: "100vw",
