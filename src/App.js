104cdead48d3d4965c420723cc54dd8513deaa52
Clemens Bülow <clemensbify@gmail.com>
2023-01-20 15:56:42 +0100

    yes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
             <GameBoard />
             <Footer />
             <Snowfall
-                snowflakeCount={270}
+                snowflakeCount={280}
                 style={{
                     position: "fixed",
                     width: "100vw",
